df40a42b95c2d61f3a9109370829766c2cac3888
pj <petejw@theswamp.co.uk>
2019-11-09 14:29:29 +0000

    Initial commit

---
+++ HelloMeadow/App.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Hardware;
+
+namespace BasicHelloMeadow
+{
+    public class App : App<F7Micro, App>
+    {
+        IDigitalOutputPort redLed;
+        IDigitalOutputPort blueLed;
+        IDigitalOutputPort greenLed;
+
+        List<PwmLed> _leds;
+
+        public App()
+        {
+            ConfigurePorts();
+
+            //var pushButton = new PushButton(Device, Device.Pins.D08);
+            //pushButton.Clicked += PushButton_Clicked;
+            BlinkLeds();
+            //Thread.Sleep(Timeout.Infinite);
+        }
+
+        private void PushButton_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Click");
+        }
+
+        public void ConfigurePorts()
+        {
+            Console.WriteLine("Creating Outputs...");
+            redLed = Device.CreateDigitalOutputPort(Device.Pins.OnboardLedRed);
+            blueLed = Device.CreateDigitalOutputPort(Device.Pins.OnboardLedBlue);
+            greenLed = Device.CreateDigitalOutputPort(Device.Pins.OnboardLedGreen);
+
+            _leds = new List<PwmLed>()
+            {
+                new PwmLed(Device.CreatePwmPort(Device.Pins.D08), TypicalForwardVoltage.Red),
+                new PwmLed(Device.CreatePwmPort(Device.Pins.D07), TypicalForwardVoltage.Green),
+                new PwmLed(Device.CreatePwmPort(Device.Pins.D06), TypicalForwardVoltage.Blue),
+                new PwmLed(Device.CreatePwmPort(Device.Pins.D05), TypicalForwardVoltage.Yellow)
+            };
+            
+        }
+
+        public void BlinkLeds()
+        {
+            var state = false;
+
+            while (true)
+            {
+                state = !state;
+
+                Console.WriteLine($"State: {state}");
+
+                redLed.State = state;
+                Thread.Sleep(500);
+                blueLed.State = state;
+                Thread.Sleep(500);
+                greenLed.State = state;
+                Thread.Sleep(500);
+
+                foreach(var led in _leds)
+                {
+                    led.IsOn = state;
+                    Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
---
+++ HelloMeadow/Program.cs
@@ -0,0 +1,17 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace BasicHelloMeadow
+{
+    class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            // instantiate and run new meadow app
+            app = new App();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
